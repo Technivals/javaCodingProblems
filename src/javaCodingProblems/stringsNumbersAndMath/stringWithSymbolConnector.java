1c1ec6ad85a8dada176fa06c871e3faa98bdb655
Technivals <shtangretm7@gmail.com>
2023-01-26 14:18:05 +0200

    Added new code

---
+++ src/javaCodingProblems/stringsNumbersAndMath/stringWithSymbolConnector.java
@@ -0,0 +1,22 @@
+package javaCodingProblems.stringsNumbersAndMath;
+
+import java.util.Scanner;
+
+public class stringWithSymbolConnector {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter line with which you want to work");
+        String [] line = sc.nextLine().split(" ");
+        System.out.println("Enter symbol with which you want connect your strings");
+        String symbol = sc.nextLine();
+        StringBuilder withSymbolConnector = new StringBuilder();
+        for (int i = 0; i < line.length; i++){
+            withSymbolConnector.append(line[i]);
+            if(i != line.length - 1) {
+                withSymbolConnector.append(symbol);
+            }
+        }
+        System.out.print("Your new line looks this: " + "\n" + withSymbolConnector);
+    }
+}
