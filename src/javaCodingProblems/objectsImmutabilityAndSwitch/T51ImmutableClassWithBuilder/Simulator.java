0ab80b54f1e29f6799a1ef0218ef430a97a1f48f
Technivals <shtangretm7@gmail.com>
2023-09-24 17:00:30 +0300

    Added minor changes

--- src/javaCodingProblems/objectsImmutabilityAndSwitch/T51ImmutableClassWithBuilder/Simulator.java
+++ src/javaCodingProblems/objectsImmutabilityAndSwitch/T51ImmutableClassWithBuilder/Simulator.java
@@ -3,11 +3,11 @@ package javaCodingProblems.objectsImmutabilityAndSwitch.T51ImmutableClassWithBui
 public class Simulator {
 
     public static void main(String[] args) {
-        Application application1 = Application.getBuilder("koka", "kokovenko", 1).build();
-        Application application2 = Application.getBuilder("kekes", "kekesovych", 2).specialisation("Cyber")
+        Application application1 = Application.getBuilder("Josh", "Martin", 1).build();
+        Application application2 = Application.getBuilder("Jack", "Hardy", 2).specialisation("Cyber")
                 .build();
-        Application application3 = Application.getBuilder("man", "pan", 3)
-                .specialisation("Economy").grade("bachelor").build();
+        Application application3 = Application.getBuilder("David", "Cross", 3).specialisation("Economy")
+                .grade("bachelor").build();
         System.out.println(application1.toString());
         System.out.println(application2.toString());
         System.out.println(application3.toString());
